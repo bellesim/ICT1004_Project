84130032bb0ae21722adadd28671a67336b528e0
Belle Sim <bellesimbeier@gmail.com>
2020-11-17 11:28:47 +0800

    added session to profile

--- profile.php
+++ profile.php
@@ -1,3 +1,19 @@
+<!-- <?php 
+$msg = "Please log in first.";
+include 'authentication.php';
+
+if (!isset($_SESSION['UserID'])) {
+
+    echo ("<script type='text/javascript'>
+        alert('$msg')
+        location.href='index.php';</script>");
+} 
+
+$UserID = $_SESSION['UserID'];
+$UserName = $_SESSION['UserName']; 
+$FullName = $_SESSION['FullName']; 
+$Email = $_SESSION['Email'];
+?> -->
 <!DOCTYPE html>
 
 <html>
@@ -16,8 +32,22 @@
     <body>
 
         <div class="container">
-            <p>Enter your contents here for My Profile! :) </p>
-        </div>            
+        <h2>Profile</h2>
+            <div class="profileInfoContainer">
+                <div class="infoDetails">
+                    <h3>Full Name</h3>
+                    <p><?php echo $FullName; ?></p>
+                </div>
+                <div class="infoDetails">
+                    <h3>Username</h3>
+                    <p><?php echo $UserName; ?></p>
+                </div>
+                <div class="infoDetails">
+                    <h3>Email</h3>
+                    <p><?php echo $Email; ?></p>
+                </div>
+            </div>
+        </div>        </div>            
             
         
         
