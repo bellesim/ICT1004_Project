bffcddf8136d63eee521203cb83970a2e61a68a3
lsf1999 <61367983+lsf1999@users.noreply.github.com>
2020-11-21 13:22:10 +0800

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -3,22 +3,33 @@ session_start();
 // session_regenerate_id();
 include 'dbFunctions.php';
 
-function check_fullname($input) {  //using regular expression to check string, only can use characters and space with min 1 - max 50
-    if (!preg_match("/^[a-zA-Z ]{1,50}+$/", $input)) {
+        
+// validate name 
+// condition: do not contain space only (e.g. "     ")
+//          : must be alphabet (no numbers/special characters are allowed)  
+//          : min 1 - max 50
+function check_fullname($input) {
+    if ((strlen(str_replace(' ', '', $input)) == 0) || (!preg_match("/^[a-zA-Z ]{1,50}+$/", $input))) {
         return false;
     } else {
         return true;
     }
 }
 
+// validate password
+// condition: MUST NOT contain space
+//          : contain at least 8 characters, including 1 uppercase, 1 lowercase, 1 number and 1 special character
+//          : max characters = 255        
 function check_password($input) {
-    if (preg_match("/^[a-zA-Z0-9@*#_ ]{8,255}+$/", $input)) {
+    if (preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,255}$/", $input)) {
         return true;
     } else {
         return false;
     }
 }
 
+// validate NRIC/FIN
+// condition: is in correct format start with S/T/F/G, follow by 7 digits and end with an alphabet
 function check_NRIC($input) {
     if (!preg_match("/^[STFG]\d{7}[A-Z]$/", $input)) {
         return false;
@@ -27,13 +38,46 @@ function check_NRIC($input) {
     }
 }
 
+// validate email with filter_var function (to double confirm)
 function check_email($input) {
-    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $input)) {
+    if (!filter_var($input, FILTER_VALIDATE_EMAIL)) {
         return false;
     } else {
         return true;
     }
 }
+
+// validate contact number
+// condition: 8 digits only and the 1st digit must be 6/8/9
+function check_contact($input) {
+    if (!preg_match("/^(6|8|9)\d{7}$/",$input)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// validate weight or height
+// condition: do not contain space only (e.g. "     ")
+//          : must be integer or double (for double, only 1 decimal point is allowed)
+function check_double_format($input){
+    if((strlen(str_replace(' ', '', $input)) == 0)||(!preg_match('/^[0-9]+(\.[0-9]{1})?$/', $input))){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// validate allergies
+// condition: do not contain space only (e.g. "     ")
+function check_allergies($input){
+    if(strlen(str_replace(' ', '', $input)) == 0){
+        return false;
+    }else{
+        return true;
+    }
+}
+
 function authenticate($nric, $enteredpassword) {
    $link = db();
    if(empty($nric) || empty($enteredpassword)){
