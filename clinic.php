fd33d76df0273d9cc000c8d81b1018b998c34dae
Belle Sim <bellesimbeier@gmail.com>
2020-12-02 18:58:14 +0800

    clinic updated

--- clinic.php
+++ clinic.php
@@ -6,7 +6,7 @@ session_start();
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Clinics/title>
+        <title>Clinics</title>
         <link rel="icon" href="img/favicon.ico">
         <!-- CSS FILES -->
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/uikit@latest/dist/css/uikit.min.css">
@@ -35,8 +35,7 @@ session_start();
                <div class="uk-container uk-text-center uk-section  ">
                 <h2 class="text-4xl font-bold"> Clinics</h2>
             </div>
-            <div class="uk-child-width-1-4@m px-24" uk-grid >
-
+            <div class="uk-child-width-1-3@m px-24" uk-grid >
                 <?php
                 $conn = new mysqli("localhost", "sqldev", "P@ssw0rd", "clinic");
 
@@ -46,29 +45,42 @@ session_start();
                 $result = $conn->query("SELECT * FROM clinic.Clinic");
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) { {
-                            ?>
-                            <div>
-                                <div class="uk-card uk-card-default">
-                                    <div class="uk-card-media-top">
-                                        <?php echo "<img src='images/".$row['Image']."'>"?>
-                                    </div>
-                                    <div class="uk-card-body">
-                                        <h3 class="text-xl font-semibold clinicTitle"><?php echo $row['ClinicName'] ?></h3>
-                                        <p class="text-base font-normal"><?php echo $row['Descriptions'] ?></p>!
-                                        <p class="text-base font-normal"><?php echo $row['ClinicAddress'] ?></p>
-                                        <p class="text-base font-normal"><?php echo $row['ClinicPostalCode'] ?></p>
-                                        <p class="text-base font-normal"><?php echo $row['ClinicContactNo'] ?></p>
+                            ?>	<div>
+									<div class="uk-card uk-card-default uk-card-small" style="box-shadow: none;">
+											<div class="uk-card-media-top">
+												<a href="#"><img data-src="https://unsplash.it/620/350/?random=4" width="620" height="350" data-uk-img="target: !.uk-slideshow-items" alt=""></a>
 
-                                        <form action="appt_booking.php" method="POST">
-                                            <input type="hidden" id="ClinicID" name="ClinicID" value=" <?php echo $row["ClinicID"]; ?> ">
-                                            
-                                    <button onclick="window.location.href = 'appt_booking.php'" class="uk-button uk-button-primary uk-width-1-1 rounded h-12 bg-blue-800" href="#modal-overflow">Make appointment</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
+											</div>
+											<div class="uk-card-header">
+												<div class="uk-grid-small uk-flex-middle" data-uk-grid>
+													<div class="uk-width-auto">
+													</div>
+													<div class="uk-width-default">
+														<h6 class="uk-margin-remove-bottom uk-text-bold mt-4"><?php echo $row['ClinicName'] ?></h6>
+													</div>
+												</div>
+											</div>
+											<div class="uk-card-body">
+                                            <div class="flex pr-4">
+                                            <p class="text-base font-semibold mr-4">Address</p>
+                                            <span class="text-base"><?php echo $row['ClinicAddress'] ?></span>
+                                            </div>
+                                            <div class="flex">
+                                            <p class="text-base font-semibold mr-4 ">Opening Hours</p>
+                                            <span class="text-base"><?php echo $row['ClinicOpeningHours'] ?></span>
+                                            </div>
+                                                <form action="appt_booking.php" method="POST">
+                                                 <input type="hidden" id="ClinicID" name="ClinicID" value=" <?php echo $row["ClinicID"]; ?> ">
+                                              <a class="uk-button uk-button-text uk-text-bold uk-margin-small" href="appt_booking.php">Book Appointment</a>
+                                                </form>
+											</div>
+										</div>
+									</div>
+                                
 
-
+                                              
+                             
+                                
                             <?php
                         }
                     }
--- head.inc.php
+++ head.inc.php
@@ -10,6 +10,7 @@
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit-icons.min.js"></script>
 
+
 <link rel="stylesheet" href="css/main.css">
 
 <title>Clinic Finder</title>
